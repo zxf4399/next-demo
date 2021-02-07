e42ba0cd400db5afe99718278fd7f299b9456909
郑贤锋 <zhengxf@tuya.com>
2021-02-07 20:58:08 +0800

    fix: typescript svg type error

--- pages/svg-components/index.tsx
+++ pages/svg-components/index.tsx
@@ -3,7 +3,6 @@ import Cat from '../../svgs/cat.svg';
 export default function Home() {
   return (
     <div className="container">
-      <marquee>SVG Cat!</marquee>
       <Cat />
       <style jsx>{`
         .container {
