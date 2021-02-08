2e98b6f58723ddeea7bb6c51bad500247a9b6062
郑贤锋 <zhengxf@tuya.com>
2021-02-08 11:46:26 +0800

    feat: add with-loading nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,28 @@
 import { AppProps } from 'next/app';
+import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import React from 'react';
 
 import '../styles/globals.css';
 import './index.scss';
 
+Router.events.on('routeChangeStart', () => NProgress.start());
+
+Router.events.on('routeChangeComplete', () => NProgress.done());
+
+Router.events.on('routeChangeError', () => NProgress.done());
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <nav>
+        <Link href="/svg-components">svg-components</Link>
+        <Link href="/with-next-sass">with-next-sass</Link>
+        <Link href="/with-next-sitemap">with-next-sitemap</Link>
+        <Link href="/with-tailwindcss">with-tailwindcss</Link>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://unpkg.com/nprogress@0.2.0/nprogress.css"
+            rel="stylesheet"
+            type="text/css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
