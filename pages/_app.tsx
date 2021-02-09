53cf68be3022e0a2f74efbdda40bae851d3f3bd2
郑贤锋 <zhengxf@tuya.com>
2021-02-09 16:26:34 +0800

    feat: add material-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 import { AppProps } from 'next/app';
 import Link from 'next/link';
 import Router from 'next/router';
 import { DefaultSeo } from 'next-seo';
 import NProgress from 'nprogress';
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import SEO from '../next-seo.config';
-
-import '../scripts/wdyr';
+import SEO from 'next-seo.config';
+import 'scripts/wdyr';
+import theme from 'theme';
 
 import '../styles/globals.css';
 import './index.scss';
@@ -19,16 +21,28 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <>
       <DefaultSeo {...SEO} />
-      <nav>
-        <Link href="/svg-components">svg-components</Link>
-        <Link href="/with-next-sass">with-next-sass</Link>
-        <Link href="/with-next-sitemap">with-next-sitemap</Link>
-        <Link href="/with-tailwindcss">with-tailwindcss</Link>
-      </nav>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <nav>
+          <Link href="/svg-components">svg-components</Link>
+          <Link href="/with-next-sass">with-next-sass</Link>
+          <Link href="/with-next-sitemap">with-next-sitemap</Link>
+          <Link href="/with-tailwindcss">with-tailwindcss</Link>
+        </nav>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
