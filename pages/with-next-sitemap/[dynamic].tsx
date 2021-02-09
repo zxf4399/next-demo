fb84e9b75d1814566a7e93e85397e3a9f63fe3bb
郑贤锋 <zhengxf@tuya.com>
2021-02-09 16:52:16 +0800

    chore: next-sitemap 10000 -> 10

--- pages/with-next-sitemap/[dynamic].tsx
+++ pages/with-next-sitemap/[dynamic].tsx
@@ -23,7 +23,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    paths: [...Array(10000)].map((_, index) => ({
+    paths: [...Array(10)].map((_, index) => ({
       params: {
         dynamic: `page-${index}`,
       },
