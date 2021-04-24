be3a13b8d8cb936510f1682f86efa26434401823
郑贤锋 <zhengxf@tuya.com>
2021-04-24 16:39:21 +0800

    feat: support webpack5

--- next.config.js
+++ next.config.js
@@ -5,16 +5,23 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
-module.exports = withPlugins([
-  [withBundleAnalyzer],
+module.exports = withPlugins(
   [
-    withPWA,
-    {
-      pwa: {
-        disable: process.env.NODE_ENV === 'development',
-        dest: 'public',
-        runtimeCaching,
+    [withBundleAnalyzer],
+    [
+      withPWA,
+      {
+        pwa: {
+          disable: process.env.NODE_ENV === 'development',
+          dest: 'public',
+          runtimeCaching,
+        },
       },
-    },
+    ],
   ],
-]);
+  {
+    future: {
+      webpack5: true,
+    },
+  }
+);
